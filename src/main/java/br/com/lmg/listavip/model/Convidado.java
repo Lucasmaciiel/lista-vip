ae8ff2b742223b1d21a9c819f36ce7bd8a63c95f
Lucas Maciel <lucasmacielgois@gmail.com>
2020-07-31 17:43:37 -0300

    Usando CRUD genérico para salvar

--- src/main/java/br/com/lmg/listavip/model/Convidado.java
+++ src/main/java/br/com/lmg/listavip/model/Convidado.java
@@ -1,4 +1,61 @@
 package br.com.lmg.listavip.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name = "convidado")
 public class Convidado {
+
+    @javax.persistence.Id
+    @GeneratedValue
+    private Long Id;
+
+    private String nome;
+
+    private String email;
+
+    private String telefone;
+
+    public Convidado(){
+
+    }
+
+    public Convidado(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
--- src/main/java/br/com/lmg/listavip/repository/ConvidadoRepository.java
+++ src/main/java/br/com/lmg/listavip/repository/ConvidadoRepository.java
@@ -1,4 +1,9 @@
 package br.com.lmg.listavip.repository;
 
-public class ConvidadoRepository {
+import br.com.lmg.listavip.model.Convidado;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ConvidadoRepository extends CrudRepository<Convidado, Long> {
+
+
 }
